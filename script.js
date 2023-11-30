8420a7adb1b0c4143aad955919b9c6941faee798
Petra Hermanová <petra.dorotikova@gmail.com>
2023-11-30 21:42:33 +0100

    hotovo

--- script.js
+++ script.js
@@ -9,3 +9,16 @@
 // document.body.innerHTML += fillcut('petr', 3) + '<br>'; // vypíše „pet“
 // document.body.innerHTML += fillcut('petr', 4) + '<br>'; // vypíše „petr“
 
+const fillcut = (str, len) => {
+    if (str.length === len) {
+        return str;
+    } else if (str.length < len) {
+        return str.padStart(len, ".");
+    } else {
+        return str.slice(0,len);
+    }
+}
+
+document.body.innerHTML += fillcut('petr', 8) + '<br>'; // vypíše „....petr“
+document.body.innerHTML += fillcut('petr', 3) + '<br>'; // vypíše „pet“
+document.body.innerHTML += fillcut('petr', 4) + '<br>'; // vypíše „petr“
